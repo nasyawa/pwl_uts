9e43fe823f30209e931b5d966f4ff4a6d89e998a
NasyawaRamadhiaKiranaSavaEffendi <ramadhianasyawa@gmail.com>
2023-04-12 13:45:02 +0700

    commit 1

---
+++ database/migrations/2023_04_12_062401_create_detail_transaksi_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_transaksi', function (Blueprint $table) {
+            $table->string('id_detail','10')->primary()->autoIncrement();
+            $table->string('id_transaksi','10')->index();
+            $table->string('id_buku','10')->index();
+            $table->integer('jumlah');
+            $table->double('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_transaksi');
+    }
+};
